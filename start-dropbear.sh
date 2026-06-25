df46ed2f96a5bc324c75c06369621c80532e0147
vialan-art <vialan.hzh@gmail.com>
2026-06-25 23:21:28 +0800

    Allow root password login for dropbear

--- start-dropbear.sh
+++ start-dropbear.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-# 手动启动 dropbear 绕过 init 脚本问题
+# 手动启动 dropbear，允许 root 密码登录
 PORT=44322
 
 # 确保停止任何已有 dropbear 进程
 pkill dropbear || true
 sleep 1
 
-# 直接启动 dropbear
-/usr/sbin/dropbear -p $PORT -E -F -w -s &
+# 直接启动 dropbear，去掉 -s 允许密码登录
+/usr/sbin/dropbear -p $PORT -E -F -w &
 
 # 放行防火墙
 ufw allow $PORT/tcp
