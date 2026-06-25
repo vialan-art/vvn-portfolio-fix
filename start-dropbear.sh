2cdf3608df9ef63cc60f62884a7cbf5e9279aadd
vialan-art <vialan.hzh@gmail.com>
2026-06-25 23:52:23 +0800

    Remove -w flag to allow root password login

--- start-dropbear.sh
+++ start-dropbear.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-# 手动启动 dropbear，允许 root 密码登录
 PORT=44322
 
-# 确保停止任何已有 dropbear 进程
+# 杀掉现有 dropbear
 pkill dropbear || true
 sleep 1
 
-# 直接启动 dropbear，去掉 -s 允许密码登录
-/usr/sbin/dropbear -p $PORT -E -F -w &
+# 启动 dropbear：允许 root 密码登录，不带 -w
+/usr/sbin/dropbear -p $PORT -E -F &
 
 # 放行防火墙
 ufw allow $PORT/tcp
